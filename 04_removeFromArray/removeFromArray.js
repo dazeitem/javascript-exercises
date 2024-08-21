b05c9180f4569d1435a58574a8c2f7ead707731e
Keenan <82282404+dazeitem@users.noreply.github.com>
2024-08-21 19:44:21 +0200

    Exercise 5 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function(value) {
-    
+const removeFromArray = function(values, ...toRemove) {
+    let finalArray = []
+
+    for (let i = 0; i < values.length; i++) {
+        if (!toRemove.includes(values[i])) {
+            finalArray.push(values[i]);
+        }
+    }  
+    return finalArray
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(...nums) {
+    let numArray = [];
+    let numSum = 0;
+    for (let element of nums) {
+        if (!Number.isInteger(element) || Array.isArray(element) || element < 0) {
+            return 'ERROR';
+        } else {
+            numArray.push(element);
+        }
+    };
+    numArray.sort((a, b) => a - b);
+    for (let i = numArray[0]; i <= numArray[1]; i++) {
+        numSum += i;
+    };
+    return numSum
 };
 
 // Do not edit below this line
