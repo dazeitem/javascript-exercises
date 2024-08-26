fd9f72446cdb7356aad7fe853c12a931ea3af272
Keenan <82282404+dazeitem@users.noreply.github.com>
2024-08-26 18:10:18 +0200

    feat: Loop through object keys and calculate oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    // Initialize variables
+    let oldestPerson;
+    let oldestRecordedAge = 0;
+    let objectAge = 0;
 
+    // Loop through the objects and find the oldest person.
+    for (let obj of arr) {
+        objectAge = obj.yearOfDeath - obj.yearOfBirth;
+        if (objectAge > oldestRecordedAge) {
+            oldestPerson = obj;
+            oldestRecordedAge = objectAge
+        };
+    };
+    return oldestPerson;
 };
 
 // Do not edit below this line
