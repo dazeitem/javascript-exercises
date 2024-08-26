1307722ffd01937a2af51506ec637fc178525a42
Keenan <82282404+dazeitem@users.noreply.github.com>
2024-08-26 09:10:25 +0200

    feat: Extract titles from dictionary keys and store in array
    - Added a loop to iterate over dictionary keys
    - Pushed each key's title property into an array
    - Returned the array containing all titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(dict) {
+    arr = []
+    for (let key of dict) {
+        arr.push(key.title);
+    };
+    return arr
 };
 
 // Do not edit below this line
