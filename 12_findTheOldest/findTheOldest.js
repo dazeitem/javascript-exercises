96214e50cbcb2c2dfe94105c2d4e3386afc25360
Keenan <82282404+dazeitem@users.noreply.github.com>
2024-08-26 18:12:57 +0200

    feat: Check if necessary properties are present in all objects
    - added function to isolate the objects lacking a yearOfDeath key

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -4,6 +4,15 @@ const findTheOldest = function(arr) {
     let oldestRecordedAge = 0;
     let objectAge = 0;
 
+    // Check if yearOfDeath is present.
+    let filteredObjects = arr.reduce((acc, obj) => {
+        const hasKey = [yearOfDeath].some(key => key in obj);
+        if (!hasKey) {
+          acc.push(obj);
+        }
+        return acc;
+    }, []); // [] indicates the initial value for the reduce method accumulator
+
     // Loop through the objects and find the oldest person.
     for (let obj of arr) {
         objectAge = obj.yearOfDeath - obj.yearOfBirth;
