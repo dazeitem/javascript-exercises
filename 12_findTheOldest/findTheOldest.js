2991ff094bf31d01615c934b83dc4275bc22fdc8
Keenan <82282404+dazeitem@users.noreply.github.com>
2024-08-27 11:38:47 +0200

    feat: objectAge for objects w/o yearOfDeath key calculated separately
    - living and non-living are put into different arrays
    - objects without yearOfDeath are assigned property "stillAlive"
    - stillAlive property given current year value
    - objectAge calculated the same loop, but with currentDate instead of yearOfDeath

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,26 +1,44 @@
 const findTheOldest = function(arr) {
     // Initialize variables
+    let peoplePassed = []
     let oldestPerson;
     let oldestRecordedAge = 0;
     let objectAge = 0;
 
     // Check if yearOfDeath is present.
     let filteredObjects = arr.reduce((acc, obj) => {
-        const hasKey = [yearOfDeath].some(key => key in obj);
+        const hasKey = ["yearOfDeath"].some(key => key in obj);
         if (!hasKey) {
           acc.push(obj);
+        } else {
+            peoplePassed.push(obj)
         }
         return acc;
     }, []); // [] indicates the initial value for the reduce method accumulator
 
+    // Find current date and add that as temporary year of death to calc age
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    for (let obj of filteredObjects) {
+        obj.currentDate = year
+    }
+
+
     // Loop through the objects and find the oldest person.
-    for (let obj of arr) {
+    for (let obj of peoplePassed) {
         objectAge = obj.yearOfDeath - obj.yearOfBirth;
         if (objectAge > oldestRecordedAge) {
             oldestPerson = obj;
             oldestRecordedAge = objectAge
         };
     };
+    for (let obj of filteredObjects) {
+        objectAge = obj.currentDate - obj.yearOfBirth;
+        if (objectAge > oldestRecordedAge) {
+            oldestPerson = obj;
+            oldestRecordedAge = objectAge
+        };
+    }
     return oldestPerson;
 };
 
